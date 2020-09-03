e2623c0df0d12d9848b77d43191945777dbb65d1
mcrsr0208 <mcrsr0208@gmail.com>
2020-09-03 12:19:35 +0530

    commit 3

--- src/Table.js
+++ src/Table.js
@@ -15,8 +15,11 @@ const TableBody = (props) => {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
+      <td>{row.name}</td>
+      <td>{row.job}</td>
+      <td>
+      <button onClick={() => props.removeCharacter(index)}>Delete</button>
+      </td>
       </tr>
     )
   })
@@ -24,16 +27,15 @@ const TableBody = (props) => {
   return <tbody>{rows}</tbody>
 }
 
-class Table extends Component {
-  render() {
-     const {characterData} = this.props
-    return (
-      <table className="table table-bordered table-dark">
-        <TableHeader />
-        <TableBody characterData={characterData} />
-      </table>
-    )
-  }
+const Table = (props) => {
+  const {characterData, removeCharacter} = props
+
+  return (
+    <table className="table table-bordered table-dark">
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
 }
 
 export default Table
